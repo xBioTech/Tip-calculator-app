d601b2c22265b2eb47ce857af1e1ef19dc9df6f1
xbiotech <biotech9261@gmail.com>
2023-03-12 19:46:07 +0100

    ADD possibillity that user splits the tip amount if there are more than one person

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const bill = document.querySelector(".bill-form");
 const tipPercentageButton = document.querySelectorAll(".btn");
 const tipPercentageCustom = document.querySelector(".custombtn");
-
+const amountOfPeople = document.querySelector(".people-form");
 
 let userTip = "";
+let splitTip = "";
 
 tipPercentageButton.forEach((btn) =>{
     btn.addEventListener("click", ()=>{
         userTip = +btn.textContent.replace("%", "");
         const calcResult = calcBill(+bill.value, userTip);
         console.log(calcResult);
+        splitTip = splitBill(calcResult, +amountOfPeople.value);
+        console.log(splitTip);
+
     });
 });
 
@@ -17,10 +21,17 @@ tipPercentageCustom.addEventListener("input", ()=> {
     userTip = tipPercentageCustom.value;
     const calcResult = calcBill(+bill.value, userTip);
     console.log(calcResult);
+    splitTip = splitBill(calcResult, +amountOfPeople.value);
+    console.log(splitTip);
 });
+
+
+function splitBill(num1, num2){
+    return num1 / num2;
+};
 
 function calcBill(num1, num2){
    return num1 * num2 / 100;
-}
+};
 
 
